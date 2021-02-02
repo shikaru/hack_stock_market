1faa2d1c2f300da738a27e53211622e0aba4bfb3
shikaru <s.hikaru.yakyu.0823@gmail.com>
2021-02-02 22:34:03 +0900

    indexes.py修正

--- get_stock_data/indexes.py
+++ get_stock_data/indexes.py
@@ -1,16 +1,9 @@
-from datetime 
-import pandas as pd
-from pandas_datareader.stooq import StooqDailyReader
-
-
-
-
 def get_indexes(start=datetime.datetime(2020, 1, 1),end=datetime.datetime(2020, 12, 31),brand='1305.JP',n=14,):
-  date=start-datetime.timedelta(days=26)
+  date=start-datetime.timedelta(days=40)
   stooq = StooqDailyReader(brand, start=date, end=end)
   data = stooq.read()
   #input data must be pandas dataframe
-  diff=data["Close"].diff()
+  diff=data["Close"].diff().sort_index(ascending=True)
   up=diff.copy()
   up[up<0]=0
   down=diff.copy()
@@ -20,8 +13,8 @@ def get_indexes(start=datetime.datetime(2020, 1, 1),end=datetime.datetime(2020, 
   RSI=(up_sma_14)/(down_sma_14+up_sma_14)
 
   #MacD
-  dshort=data["Close"].ewm(span=12,adjust=False).mean()
-  long=data["Close"].ewm(span=26,adjust=False).mean()
+  short=data["Close"].sort_index(ascending=True).ewm(span=12,adjust=False).mean()
+  long=data["Close"].sort_index(ascending=True).ewm(span=26,adjust=False).mean()
   macd=short-long
   signal = macd.ewm(span=9).mean()
-  return RSI[26:]*100, macd[26:],signal[26:],data["Close"]
+  return RSI[30:]*100, macd.sort_index(ascending=True)[30:],signal.sort_index(ascending=True)[30:],data["Close"][:-30].sort_index(ascending=True)
